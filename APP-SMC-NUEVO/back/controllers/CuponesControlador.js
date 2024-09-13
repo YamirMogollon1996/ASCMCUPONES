4b75091f743bd468d7e1f3345fba41ba3195ea7e
yamir mogollon <mogollonosorioyamir@gmail.com>
2024-09-13 08:57:56 -0500

    agrege la opcio para subir opcion

--- APP-SMC-NUEVO/back/controllers/CuponesControlador.js
+++ APP-SMC-NUEVO/back/controllers/CuponesControlador.js
@@ -2,9 +2,7 @@ const cupones = require("../models/Cupones");
 
 const controladorCupones =  {}
 
-
 controladorCupones.crear =  async (  req, res) =>{
-   
     return   res.send({
         msg:"error"
     })
--- APP-SMC-NUEVO/back/router/rutaCupones.js
+++ APP-SMC-NUEVO/back/router/rutaCupones.js
@@ -1,8 +1,13 @@
 const express  = require("express" )
+const upload = require("../helpers/Subirimagen")
+const controladorCupones = require("../controllers/CuponesControlador")
+const RouterCupones  = express.Router()  
 
-const RouterCupones  = express.Router()
 
- 
-RouterCupones.post("/"   )
+RouterCupones.post(
+  "/imagencupon",
+  upload.single("ImagenCupon"),
+  controladorCupones.crear
+);
 
 module.exports  = RouterCupones
